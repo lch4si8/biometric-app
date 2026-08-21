46cecf5638a81a89fbd2364c9ba4e1a365009878
lch4si8 <lchasi311@gmail.com>
2026-08-21 18:24:12 +0200

    feat: Added backend base lambda handlers

---
+++ backend/src/handlers/register.mjs
@@ -0,0 +1,11 @@
+export const handler = async (event) => {
+  return {
+    statusCode: 501,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+      'Access-Control-Allow-Credentials': 'true',
+    },
+    body: JSON.stringify({ message: 'POST /register - NotImplemented' }),
+  };
+};
